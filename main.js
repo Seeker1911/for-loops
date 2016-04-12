d8b8bd86288dc8fb5ea65cc3ae3545454bc9b7dc
Michael Mead <michaelmead007@icloud.com>
2016-04-12 08:46:28 -0500

    Add divisor loop.

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ############## for loops #####################
 
 // Write a for loop that increments the counter variable by 10 each time, and displays the value.
-var Things = 100
+var Things = 100;
 
 for (var i = 0; i < Things.length; i=i+10) {
 	Things[i]
@@ -9,13 +9,15 @@ for (var i = 0; i < Things.length; i=i+10) {
 console.log("numberOfThings:", Things);
 
 // Write a for loop that divides the counter variable by 2 each time, and displays the value.
-
-for (var i = Things.length; i > 0; i /= 2) {
-	Things[i]
+var  stuff = 0;
+for (var i = 100; i > stuff.length; i = i/=2) {
+	stuff[i];
+	var myArray = [i,];
+};
+console.log("divide by 2", stuff);
+console.log("array:", myArray);
 	// Augment the loop to insert each new value into an array
 
-};
-console.log("divide by two:", Things);
 
 // Write a loop that starts at 100 and decrements a variable by 1. 
 //If the number is even, add the number to the beginning of an array, else add it to the end of the array.
